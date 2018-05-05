725ca23209c0ed918813e6d3756553da84da2159
asebak <asebak@users.noreply.github.com>
2018-05-05 12:43:17 -0400

    Update send.js

--- send.js
+++ send.js
@@ -1,5 +1,2 @@
-web3.fromWei(web3.eth.getBalance(web3.eth.coinbase)).toString()
-
-web3.personal.unlockAccount("aion address","aion password","6000")
-
-web3.eth.sendTransaction({from: 'from address', to: 'to address', value: web3.toWei(1)})
+web3.personal.unlockAccount(eth.coinbase,"aion password") //returns true if unlock worked
+web3.eth.sendTransaction({from: eth.coinbase, to: 'to address', value: web3.toWei(1)})
